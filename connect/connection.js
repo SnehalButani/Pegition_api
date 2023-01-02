681f6c9a75956a8117c30497d83fdfe358217a92
SnehalButani <snehalbutani03@gmail.com>
2023-01-02 17:07:57 +0530

    save

--- connect/connection.js
+++ connect/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 main();
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/Comment')
+    await mongoose.connect('mongodb+srv://Snehal:12345@cluster0.pegveid.mongodb.net/Comment')
         .then(() => {console.log('mongo connect...')})
         .catch(err => console.log(err));
-}
+}   
